b09bc392963df6539269573c1b90e344b1645a24
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-06-15 13:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort();
+  let res = target+1;
+	for(let i =0;i<arr.length ;i++){
+           let temp = += arr[i];
+		if(temp < target && temp <= res){
+			res = temp ;
+		}
+		
+	}
+	return res;
 }
 
 module.exports = threeSum;
